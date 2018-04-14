5ae802ec7223201298a65ee7bdc6884f8aeeef2f
PragnaM <pragna.m97@gmail.com>
2018-04-14 12:13:16 +0000

    Added editedFrontend

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,5 +1,26 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope) {
-
+    $scope.messages = [
+        {
+            'sender': 'USER',
+            'text': 'Hello'
+	},
+        {
+            'sender': 'BOT',
+            'text': 'Hi, what can I do for you?'
+	},
+        {
+            'sender': 'USER',
+            'text': 'How are you?'
+	},
+        {
+            'sender': 'BOT',
+            'text': 'Im good, how are you?'
+	},
+        {
+            'sender': 'USER',
+            'text': 'Good'
+	}
+	];
 });
