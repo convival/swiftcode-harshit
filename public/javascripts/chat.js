83836b6b02cf34b99693f8fb9c3e71fabbc08142
Harshith <harshith@rediffmail.com>
2018-04-15 11:51:16 +0000

    final commit

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -5,27 +5,26 @@ app.config(function ($mdThemingProvider) {
         .accentPalette('blue');
 });
 
-app.controller('chatController', function ($scope) {
-    $scope.messages = [
-        {
-            'sender': 'USER',
-            'text': 'Hello'
-	},
-        {
-            'sender': 'BOT',
-            'text': 'Hi, what can I do for you?'
-	},
-        {
-            'sender': 'USER',
-            'text': 'How are you?'
-	},
-        {
-            'sender': 'BOT',
-            'text': 'Im good, how are you?'
-	},
-        {
-            'sender': 'USER',
-            'text': 'Good'
-	}
-	];
+app.controller('chatController', function ($scope, $sce) {
+    $scope.messages = [];
+    $scope.trust = $sce.trustAsHtml;
+    var exampleSocket = new WebSocket('ws://localhost:9000/chatSocket');
+
+    exampleSocket.onmessage = function (event) {
+        var jsonData = JSON.parse(event.data);
+        jsonData.time = new Date()
+            .toLocaleTimeString();
+        $scope.messages.push(jsonData);
+        $scope.$apply();
+        console.log(jsonData);
+
+    };
+
+
+    $scope.sendMessage = function () {
+        exampleSocket.send($scope.userMessage);
+        $scope.userMessage = '';
+
+    };
+
 });
